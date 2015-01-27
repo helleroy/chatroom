66945b525012867e9f1062a0fbf8fa9444880d03
Henrik Hellerøy <henrik.helleroy@bekk.no>
2015-01-27 16:28:57 +0100

    Consolidated server and client code into this project

---
+++ bin/chatclient.dart
@@ -0,0 +1,61 @@
+import "dart:io";
+import "dart:convert";
+
+class ChatClient {
+
+  WebSocket _socket;
+  String _address;
+  int _port;
+  List<ChatClient> _clients;
+
+  ChatClient(WebSocket socket, HttpConnectionInfo connectionInfo, List<ChatClient> clients) {
+    _socket = socket;
+    _address = connectionInfo.remoteAddress.address;
+    _port = connectionInfo.remotePort;
+    _clients = clients;
+
+    _socket.listen(messageHandler, onError : errorHandler, onDone : finishedHandler);
+  }
+
+  void messageHandler(String data) {
+    print("Message from $_address:$_port: $data");
+    distributeMessage("$_address:$_port Message: ${data.trim()}");
+  }
+
+  void errorHandler(String error) {
+    print("$_address:$_port: Error: $error");
+    removeClient(this);
+    _socket.close();
+  }
+
+  void finishedHandler() {
+    var message = "$_address:$_port: Disconnected";
+    print(message);
+    removeClient(this);
+    distributeMessage(JSON.encode({
+        "message": message, "connectedClients": _clients
+    }));
+    _socket.close();
+  }
+
+  void write(String message) {
+    _socket.add(message);
+  }
+
+  void distributeMessage(String message) {
+    for (ChatClient client in _clients) {
+      client.write(message);
+    }
+  }
+
+  void removeClient(ChatClient client) {
+    _clients.remove(this);
+  }
+
+  Map toJson() {
+    return {
+        "id" : "$_address:$_port"
+    };
+  }
+
+}
---
+++ bin/chatroom-server.dart
@@ -0,0 +1,43 @@
+import "dart:io";
+import "dart:convert";
+import "chatclient.dart";
+
+List<ChatClient> clients = [];
+
+void main() {
+
+  HttpServer.bind(InternetAddress.ANY_IP_V6, 8080).then((HttpServer server) {
+    server.listen((HttpRequest request) {
+      if (WebSocketTransformer.isUpgradeRequest(request)) {
+        WebSocketTransformer.upgrade(request).then(handleWS(request.connectionInfo));
+      } else {
+        request.response.statusCode = HttpStatus.FORBIDDEN;
+        request.response.reasonPhrase = "WebSocket connections only";
+        request.response.close();
+      }
+    });
+  });
+}
+
+Function handleWS(HttpConnectionInfo connectionInfo) {
+  return (WebSocket client) {
+    print("Connection from ${connectionInfo.remoteAddress.address}:${connectionInfo.remotePort}");
+
+    var json = JSON.encode({
+        "message": "${connectionInfo.remoteAddress.address}:${connectionInfo.remotePort} Connected",
+        "connectedClients": clients
+    });
+
+    ChatClient chatClient = new ChatClient(client, connectionInfo, clients);
+
+    clients.add(chatClient);
+
+    client.add("Welcome to the chat! There are ${clients.length - 1} other clients.\n");
+
+    chatClient.distributeMessage(JSON.encode({
+    "message": "${connectionInfo.remoteAddress.address}:${connectionInfo.remotePort} Connected",
+    "connectedClients": clients
+    }));
+
+  };
+}
